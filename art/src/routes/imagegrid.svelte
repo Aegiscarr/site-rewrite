e8dede592e220fcf7f7b08ca9e04693ebc2221d2
Aegiscarr <itsmecobra2006@gmail.com>
2022-10-19 18:59:37 +0200

    update things

--- art/src/routes/imagegrid.svelte
+++ art/src/routes/imagegrid.svelte
@@ -1,71 +1,59 @@
 <script>
     import {fly} from 'svelte/transition';
     import {quintOut} from 'svelte/easing';
-    import {onMount} from 'svelte'
+    import {onMount} from 'svelte';
+    import Renderitem from './renderitem.svelte';
     let imgtoggle = false;
 
     onMount(() => {
         setTimeout(function() {imgtoggle = true}, 0)
      })
+
+    
 </script>
 
 {#if imgtoggle}
 <div class="imggrid">
     <div class="renderitem" in:fly="{{duration: 500, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/Fa7omy3UIAgG1AX-preview.jpg" alt="alternate reality">
-        <h6><i>Alternate Reality</i></h6>
+        <Renderitem src="src/lib/img/preview/Fa7omy3UIAgG1AX-preview.jpg" title="Alternate Reality"/>
     </div>
 
     <div class="renderitem" in:fly="{{duration: 500, delay: 50, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/FbLfW6dUUAAXgSu-preview.jpg" alt="final showdown">
-        <h6><i>Final Showdown</i></h6>
+        <Renderitem src="src/lib/img/preview/FbLfW6dUUAAXgSu-preview.jpg" title="Final Showdown"/>
     </div>
 
     <div class="renderitem" in:fly="{{duration: 500, delay:100, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/FZ0X5mVXEAUXTuj-preview.jpg" alt="inspection">
-        <h6><i>Inspection</i></h6>
+        <Renderitem src="src/lib/img/preview/FZ0X5mVXEAUXTuj-preview.jpg" title="Inspection"/>
     </div>
 
     <div class="renderitem" in:fly="{{duration: 500, delay:150, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/aegis-final-a.png" alt="Render of Aegis.">
-        <h6><i>Draconia // Aegis</i></h6>
+        <Renderitem src="src/lib/img/preview/aegis-final-a.png" title="Draconia // Aegis"/>
     </div>    
     
     <div class="renderitem" in:fly="{{duration: 500, delay:200, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/exaen-final-a.png" alt="Render of Exaen.">
-        <h6><i>Draconia // Exaen</i></h6>
+        <Renderitem src="src/lib/img/preview/exaen-final-a.png" title="Draconia // Exaen"/>
     </div>
 
     <div class="renderitem" in:fly="{{duration: 500, delay:250, y: 20, easing:quintOut}}">
-        <img src="src/lib/img/preview/bloody-hell.png" alt="Bloody Hell, mate.">
-        <h6><i>Bloody Hell</i></h6>
+        <Renderitem src="src/lib/img/preview/bloody-hell.png" title="Bloody Hell"/>
     </div>
 </div>
 {/if}
 
 
 <style>
-    img {
-        width: 25rem;
-        margin-right: 1rem;
-        margin-bottom: 1rem;
-        text-align: center;
-    }
 
     .imggrid {
-        margin-top: 12.5rem;
+        margin-top: 7rem;
         display: flex;
         justify-content: center;
         flex-wrap: wrap
     }
 
-    h6 {
-        color: white;
-        font-family: 'Exo 2', sans-serif;
-        font-size: 18px;
-        font-weight: 300;
-        text-align: center;
-        margin-top: 16px
+    .renderitem {
+        width: 25%;
+        margin-right: 1rem;
+        margin-bottom: 1rem;
     }
 
     * {
--- art/src/routes/navbar.svelte
+++ art/src/routes/navbar.svelte
@@ -17,7 +17,9 @@
     let pageColors = ['#511c55', '#5a0070', '#ff9900', '#00aa00']
 </script>
 
+
 {#if showNavbarLogo}
+<div class="navbar noscroll">
 <svg id='navbarlogo' class=noscroll xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
 		<g opacity={logo_opacity} in:fly="{{duration: 250, y: -20}}" out:fly="{{duration: 250, y: -20}}">
 			<path
@@ -37,6 +39,7 @@
 		</path>
 		</g>
 </svg>
+</div>
 {/if}
 
 <style>
@@ -46,14 +49,22 @@
         height: 100%;
         position: absolute;
         scale: 25%;
-        bottom: 40%;
+        bottom: 41%;
         z-index: 5;
 		background: transparent;
+        display: flex
     }
 
 	rect {
 		fill:white;
 		z-index: 6;
 	}
+
+    .navbar {
+        width: 100%;
+        height: 12%;
+        background-color: #00a000;
+        top: 0%
+    }
 
 </style>
